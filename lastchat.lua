6178491061bf748432309c1d54102540b2b31f05
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2022-06-02 05:27:44 -0500

    Create lastchat.lua

---
+++ lastchat.lua
@@ -0,0 +1,28 @@
+function lasc()
+bc = BrickColor.new("Lime green")
+
+game.StarterGui:SetCore("ChatMakeSystemMessage", {
+
+Text = "[Scripter][Matrixplayer]: Well guys, this will be the last update of the gui, at least for now, I don't know if I'll update it in the future, but well I have to focus on other things, it was a pleasure ;3.";
+
+Font = Enum.Font.Highway;
+
+Color = bc.Color;
+
+FontSize = Enum.FontSize.Size8;
+})
+wait(10)
+bc = BrickColor.new("New Yeller")
+
+game.StarterGui:SetCore("ChatMakeSystemMessage", {
+
+Text = "[👑Admin👑][Jasboy]: OH NO!!";
+
+Font = Enum.Font.Highway;
+
+Color = bc.Color;
+
+FontSize = Enum.FontSize.Size8;
+})
+end 
+delay(1,lasc())
