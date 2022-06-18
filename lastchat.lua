1a5dd1daf8481bbd35dd91ebeefe90f3afc70008
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2022-06-18 05:39:25 -0500

    Update lastchat.lua

--- lastchat.lua
+++ lastchat.lua
@@ -1,15 +1,13 @@
 
-bc = BrickColor.new("Toothpaste")
-
-game.StarterGui:SetCore("ChatMakeSystemMessage", {
-
-Text = "[Scripter][Matrixplayer]: OwO";
-
-Font = Enum.Font.Highway;
-
-Color = bc.Color;
-
-FontSize = Enum.FontSize.Size8;
-})
+coroutine.wrap(function()
+   wait(2)
+    bc = BrickColor.new("Toothpaste")
+    game.StarterGui:SetCore("ChatMakeSystemMessage", {
+    Text = "[Scripter][Matrixplayer]: OwO ";
+    Font = Enum.Font.Highway;
+    Color = bc.Color;
+    FontSize = Enum.FontSize.Size8;
+     })
+  end)()
 
 
