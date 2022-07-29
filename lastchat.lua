aafc763ad437274a61ec635059e14b0636fa7e58
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2022-07-28 19:35:59 -0500

    Update lastchat.lua

--- lastchat.lua
+++ lastchat.lua
@@ -1,9 +1,9 @@
 
 coroutine.wrap(function()
    wait(2)
-    bc = BrickColor.new("Toothpaste")
+    bc = BrickColor.new("Lime green")
     game.StarterGui:SetCore("ChatMakeSystemMessage", {
-    Text = "[Scripter][Matrixplayer]: OwO ";
+    Text = "[Scripter][Matrixplayer]: I'm working on changing a few things and adding more games ";
     Font = Enum.Font.Highway;
     Color = bc.Color;
     FontSize = Enum.FontSize.Size8;
