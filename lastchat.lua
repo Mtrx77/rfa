2474761affbec7e75b65c22c0a922acc9ceab586
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2023-02-23 18:00:26 -0600

    Update lastchat.lua

--- lastchat.lua
+++ lastchat.lua
@@ -1,4 +1,11 @@
 coroutine.wrap(function()
+    wait(1)
+    bc = BrickColor.new("Hot pink")
+   game.StarterGui:SetCore("ChatMakeSystemMessage", {
+   Text = "[Scripter][Matrixplayer]:UwU ";
+   Font = Enum.Font.Highway;
+   Color = bc.Color;
+   FontSize = Enum.FontSize.Size8;
+    })
 
- 
   end)()
