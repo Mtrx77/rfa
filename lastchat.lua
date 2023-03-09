6deccc3ab9d3426b40c04cadaace007bd0df5d23
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2023-03-09 15:24:02 -0600

    Update lastchat.lua

--- lastchat.lua
+++ lastchat.lua
@@ -1,3 +1,10 @@
 coroutine.wrap(function()
-
-  end)()
+    wait(1)
+    bc = BrickColor.new("Deep orange")
+    game.StarterGui:SetCore("ChatMakeSystemMessage", {
+    Text = "[System]: Legends of speed has been added to solo hub";
+    Font = Enum.Font.Highway;
+    Color = bc.Color;
+    FontSize = Enum.FontSize.Size8;
+     })
+   end)()
