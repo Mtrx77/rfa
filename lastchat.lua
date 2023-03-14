bc36979b53772fba9ce1648f1407c94822db6aca
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2023-03-14 17:04:19 -0600

    Update lastchat.lua

--- lastchat.lua
+++ lastchat.lua
@@ -1,3 +1,10 @@
 coroutine.wrap(function()
-
+    wait(1)
+    bc = BrickColor.new("Really red")
+    game.StarterGui:SetCore("ChatMakeSystemMessage", {
+    Text = "[System]: It seems that the host has some issues";
+    Font = Enum.Font.Highway;
+    Color = bc.Color;
+    FontSize = Enum.FontSize.Size8;
+     })
    end)()
