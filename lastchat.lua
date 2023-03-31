3d0180b8103ce14c00f03b7a8a4611558b933d44
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2023-03-31 13:00:54 -0600

    Update lastchat.lua

--- lastchat.lua
+++ lastchat.lua
@@ -1,2 +1,2 @@
 local chatfunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mtrx77/slhub/main/chatfunc.lua"))()
-chatfunc.chatsystem("Super evolution added to solo hub","Lime green")
+chatfunc.chatsystem("Super evolution has been added to solo hub","Lime green")
