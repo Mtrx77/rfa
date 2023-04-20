d4facf0436948056339660dec8e600cc8e05eda2
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2023-04-20 13:04:51 -0600

    Update lastchat.lua

--- lastchat.lua
+++ lastchat.lua
@@ -1,2 +1,1 @@
-local chatfunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mtrx77/slhub/main/chatfunc.lua"))()
---chatfunc.chatsystem("Super evolution has been added to solo hub","Lime green")
+local chatfunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mtrx77/slhub/main/chatfunc.lua"))()chatfunc.chatsystem("It seems that there is an error in the whitelist, the problem will be solved as soon as possible","Really red")
