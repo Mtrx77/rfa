a4cb70167ab52b2f5fbd41bc9277b64c947edf95
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2023-04-20 13:05:04 -0600

    Update lastchat.lua

--- lastchat.lua
+++ lastchat.lua
@@ -1,1 +1,2 @@
-local chatfunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mtrx77/slhub/main/chatfunc.lua"))()chatfunc.chatsystem("It seems that there is an error in the whitelist, the problem will be solved as soon as possible","Really red")
+local chatfunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mtrx77/slhub/main/chatfunc.lua"))()
+chatfunc.chatsystem("It seems that there is an error in the whitelist, the problem will be solved as soon as possible","Really red")
