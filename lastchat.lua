208a9d93d26bab4e3d4cbaa9679fecca3fd71def
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2023-04-20 21:39:55 -0600

    Update lastchat.lua

--- lastchat.lua
+++ lastchat.lua
@@ -1,3 +1,3 @@
 local chatfunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mtrx77/slhub/main/chatfunc.lua"))()
---chatfunc.chatsystem("It seems that there is an error in the whitelist, the problem will be solved as soon as possible","Really red")
+chatfunc.chatsystem("Whitelist time has been changed to 24 hours",""Lime green"")
 
