f273b3c9deb02228a88eb2800bfbf164972ed576
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2023-04-21 22:05:48 -0600

    Update lastchat.lua

--- lastchat.lua
+++ lastchat.lua
@@ -1,3 +1,3 @@
 local chatfunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mtrx77/slhub/main/chatfunc.lua"))()
-chatfunc.chatsystem("Solo hub website is currently not working due to an update, if you are not whitelisted please try again later","Lime green")
+chatfunc.chatsystem("Solo Hub website has been updated correctly, if it is bugged for you it is because your cache has not been updated, press the Ctrl + F5 key combination to fix it","Lime green")
 
