27acf50bbd95fef49bb59ce7649b8ea74807188e
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2023-04-21 19:10:51 -0600

    Update lastchat.lua

--- lastchat.lua
+++ lastchat.lua
@@ -1,3 +1,3 @@
 local chatfunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mtrx77/slhub/main/chatfunc.lua"))()
-chatfunc.chatsystem("Solo hub website will be down in 1-2 hours, due to new changes","Lime green")
+chatfunc.chatsystem("Solo hub website is currently not working due to an update, if you are not whitelisted please try again later","Lime green")
 
