60abd2d0ea965cedc6d8499cac56f28d2e0b34f3
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2023-07-28 12:15:57 -0600

    Update lastchat.lua

--- lastchat.lua
+++ lastchat.lua
@@ -1,3 +1,3 @@
 local chatfunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mtrx77/slhub/main/chatfunc.lua"))()
-chatfunc.chatsystem("AFS X?","Deep orange")
+--chatfunc.chatsystem("AFS X?","Deep orange")
 
