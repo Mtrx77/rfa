c04a410a8a2c37c7e335dc9f589d9e33f840be0b
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2023-08-09 21:01:07 -0600

    Update lastchat.lua

--- lastchat.lua
+++ lastchat.lua
@@ -1,3 +1,3 @@
 local chatfunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mtrx77/slhub/main/chatfunc.lua"))()
-chatfunc.chatsystem("Afs X has been added to solo hub?","Lime green")
+chatfunc.chatsystem("Afs X has been added to solo hub","Lime green")
 
